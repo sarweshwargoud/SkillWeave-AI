2d32ab04373103172f616f593ad3500a05a2f9d5
Sarweshwar Buddolla <127119085+sarweshwargoud@users.noreply.github.com>
2026-01-18 13:30:50 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/v1';
+const API_URL = 'https://skillweave-api.onrender.com/api/v1';
 
 export const generateCourse = async (topic, level, accentPreference) => {
     try {
