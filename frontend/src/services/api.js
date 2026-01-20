1f7454ad9a9a64d410d409d8233a52664151501e
Sarweshwar Buddolla <127119085+sarweshwargoud@users.noreply.github.com>
2026-01-20 12:25:22 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://skillweave-api.onrender.com/api/v1';
+const API_URL =import.meta.env.VITE_API_URL;
 
 export const generateCourse = async (topic, level, accentPreference) => {
     try {
